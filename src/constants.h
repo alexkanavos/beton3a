14c88a7c3de134a2f4a63a5bec3ec55bc7adae1c
Alex Kanavos <alexkanv@gmail.com>
2021-04-11 23:32:06 +0300

    cleaning

--- src/constants.h
+++ src/constants.h
@@ -6,11 +6,9 @@ namespace constants
 	inline constexpr double pi{3.141592};
 	inline constexpr double g_con{25.0};
 	inline constexpr double g_soil{20.0};
-	inline constexpr double f_yk{500.0};
-	inline constexpr double f_ck{25.0};
 	inline constexpr double step{0.05};
 	inline constexpr double rho1{0.0013312};
-	inline constexpr double vrdmax{3.825};
+	inline constexpr double vrdmax{3825.0};
 	
 }
 
